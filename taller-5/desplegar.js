b98ad26aff7cf021ce7a4ddb1930e4194cb72893
pimientoyolo125 <mgefrad@gmail.com>
2023-10-12 02:22:14 -0500

    corrigiendo error al desplegar

--- taller-5/desplegar.js
+++ taller-5/desplegar.js
@@ -4,12 +4,9 @@ var checkbox_enfermedades = document.getElementById("enfermedades");
 var bandera = false;
 
 checkbox_enfermedades.addEventListener("click", function() {
-    if (bandera != false) {
-        checkbox_enfermedades.checked = !checkbox_enfermedades.checked;
-        desplegar.style.display = "none";
-        bandera = false;
-    } else {
-        bandera = true;
+    if(this.checked){
         desplegar.style.display = "block";
+    }else {
+        desplegar.style.display = "none";
     }
 })
